ad7b012b0ce26f72966433c0ea6e2064359675e3
Marvin Bitterlich <marvin.bitterlich@intercom.io>
2024-07-26 13:40:10 +0100

    support better typescript resolution without any

--- interface.d.ts
+++ interface.d.ts
@@ -5,7 +5,7 @@ interface GetLazyVar {
 type AnyFunction = (...args: any[]) => any;
 
 export const get: GetLazyVar;
-export function def(name: string, implementation: () => any): void;
+export function def<T>(name: string, implementation: () => T): () => T;
 export function sharedExamplesFor(summary: string, implementation: (...vars: any[]) => void): void;
 export function itBehavesLike(summary: string | AnyFunction, ...vars: any[]): void;
 export function includeExamplesFor(summary: string | AnyFunction, ...vars: any[]): void;
